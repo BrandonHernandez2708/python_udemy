d6ef37a54481843d96ed3d4b45c9d700d4f7d2e8
BrandonHernandez2708 <bhernandezs11@miumg.edu.gt>
2024-08-22 21:41:17 -0600

    mejorado

--- ej7.numeros impares.py
+++ ej7.numeros impares.py
@@ -1,7 +1,6 @@
 inicio=int(input("Ingrese el inicio de la cuenta "))
 final=int(input("Ingrese el final de la cuenta "))
-for numeros in range (inicio,final,+1):
-   
-    if numeros %2 :
+for numeros in range(inicio, final+1):
+    if numeros%2:
         print(numeros)
 
